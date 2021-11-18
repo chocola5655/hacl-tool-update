3b327c633f47ae70ec34784c2afd87b3cd9384fd
chocola5655 <94589977+chocola5655@users.noreply.github.com>
2021-11-18 14:08:02 +0800

    Delete 下載.cmd

--- 下載.cmd
+++
@@ -1,2 +0,0 @@
-$client = new-object System.Net.Webclient
-$client.DownloadFile('https://doc-00-2k-docs.googleusercontent.com/docs/securesc/dakbertsmieu3b44cc34gab6bvf3h1cs/dnt7bknu0ehjgvar9uaas5om0ujtk4ip/1637066400000/03457288998033307122/03457288998033307122/1xPMBqNgycjYOQKXY1lqkIkAPEuB7NRdj?e=download&authuser=0','C:\Users\win10\Desktop\新增資料夾/下載.txt')
